7f0f31654df8c9f274de16692fd5b9203199ec42
LC <lcuevastodoit@gmail.com>
2023-01-18 06:55:06 -0500

    Improving test app

---
+++ Ruby/star_wars_level_1/boot.rb
@@ -0,0 +1,6 @@
+Dir.glob('./app/**/*.rb').sort.each do |file|
+  require file
+end
+
+# to use pry console, from root, run "pry -I . -r boot.rb"
+# , then you can use console and see byebug bindings
